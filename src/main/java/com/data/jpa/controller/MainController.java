c7d9a35cc17c028a17e8ee4c93419cddedfa2fc9
SHAJAL987 <ashiqulislamshajal.cse@gmail.com>
2023-05-18 14:27:37 +0600

    feat:tomcat api added

--- src/main/java/com/data/jpa/controller/MainController.java
+++ src/main/java/com/data/jpa/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.data.jpa.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/actuator/api")
 public class MainController {
 	
+	@GetMapping("/checkHealth")
 	public String checkApiHealth() {
 		return "API Health is Ok!!";
 	}
