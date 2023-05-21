b80dba8d2d34a7254c9c74d034d19301779d2bee
SHAJAL987 <ashiqulislamshajal.cse@gmail.com>
2023-05-21 17:24:28 +0600

    feat:final subnit

--- src/main/java/com/data/jpa/SpringDataJpaProjectApplication.java
+++ src/main/java/com/data/jpa/SpringDataJpaProjectApplication.java
@@ -2,9 +2,11 @@ package com.data.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringDataJpaProjectApplication {
+public class SpringDataJpaProjectApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaProjectApplication.class, args);
--- src/main/java/com/data/jpa/controller/MainController.java
+++ src/main/java/com/data/jpa/controller/MainController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/")
 public class MainController {
 	
 	@GetMapping
 	public String checkApiHealth() {
+		return "API Health is Ok!!";
+	}
+	
+	@GetMapping("Test")
+	public String test() {
 		return "API Health is Ok!!";
 	}
 }
